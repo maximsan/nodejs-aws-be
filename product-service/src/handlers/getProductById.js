fd864453f7bb645eea15773faf0b0da01ea81a4b
maximsan <maximuzz@yandex.ru>
2020-11-08 19:10:33 +0300

    feature: integrate getProductById.js, getProductsList.js with AWS db

--- product-service/src/handlers/getProductById.js
+++ product-service/src/handlers/getProductById.js
@@ -1,15 +1,22 @@
-import {ProductsService} from "../products.service";
-import productModel from '../../products.json';
 import {NotFoundError} from "../NotFoundError";
 import {setHeaders} from "../utils/setHeaders";
+import {runDB} from "../utils/db";
 
 export const getProductById = async event => {
-    const productService = new ProductsService(productModel);
-    const {pathParameters: {id}, headers} = event;
+    const {pathParameters: {id}} = event;
+    const db = runDB();
 
     let response;
     try {
-        const product = await productService.get(id);
+        const product = await db.query({
+                text: `select s.count, p.price, p.title, p.description
+                       from products as p
+                                join stocks as s on p.id = s.product_id
+                       where p.id = $1`,
+                values: [id]
+            }
+        );
+
         response = {
             statusCode: 200,
             body: JSON.stringify(product),
---
+++ product-service/src/utils/db.js
@@ -0,0 +1,24 @@
+import {Client} from 'pga   ';
+
+const {DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USERNAME} = process.env;
+
+const options = {
+    database: DB_DATABASE,
+    host: DB_HOST,
+    port: DB_PORT,
+    password: DB_PASSWORD,
+    username: DB_USERNAME
+}
+
+export const runDB = async () => {
+    const client = new Client(options);
+    try {
+        await client.connect();
+    } catch (e) {
+        console.error(`Error during db connection establishing - ${e}`)
+    } finally {
+        client.end();
+    }
+
+    return client;
+}
