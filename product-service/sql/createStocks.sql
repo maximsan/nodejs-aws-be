50bad0a04ab4c80e3e695c8411e9c88dc32b690b
maximsan <maximuzz@yandex.ru>
2020-11-14 12:42:51 +0300

    fix: change insert stocks sql

--- product-service/sql/createStocks.sql
+++ product-service/sql/createStocks.sql
@@ -8,10 +8,10 @@ create table stocks
 ------------------------
 
 -- insert into stocks (count, product_id) values
--- (5, '90bcec32-400f-41f7-824e-06aecce29d51'),
--- (2, 'c77e2023-4e5b-43e5-94ae-3e736e8cae33'),
--- (3, '5570870e-2d2e-4d9e-a033-059e3d54a425'),
--- (1, '8f9e17bf-96c2-49e2-92d7-9a3e68224ef7'),
--- (2, '7e43c7bd-cf49-4998-a8a7-1d5414ffcbc0'),
--- (2, '7dff77e5-172c-4027-a708-2f1410de6955')
+-- (5, 'product_id'),
+-- (2, 'product_id'),
+-- (3, 'product_id'),
+-- (1, 'product_id'),
+-- (2, 'product_id'),
+-- (2, 'product_id')
 
