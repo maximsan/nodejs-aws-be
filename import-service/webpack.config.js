4da9bac8d99ca331d5339db441a8977e8b1f2785
maximsan <maximuzz@yandex.ru>
2020-11-14 13:48:36 +0300

    feat: init import-service

---
+++ import-service/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+
+module.exports = {
+    entry: slsw.lib.entries,
+    output: {
+        libraryTarget: 'commonjs',
+        filename: '[name].js',
+        path: path.join(__dirname, '.webpack'),
+    },
+    mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+    target: 'node',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                enforce: 'pre',
+                exclude: /node_modules/,
+                include: __dirname,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        targets: {
+                                            node: 'current',
+                                        },
+                                    },
+                                ],
+                            ],
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
