4eee521399d25a9ede903ec3baebff1cacb5f30f
maximsan <maximuzz@yandex.ru>
2020-11-23 01:39:28 +0300

    feat: add 2 subscriptions with filterPolicy based on price

--- product-service/src/sns.service.js
+++ product-service/src/sns.service.js
@@ -3,14 +3,20 @@ import {SNS} from "aws-sdk";
 
 
 export class SnsService {
-    async send(message) {
+    async send(messages) {
         const sns = new SNS({region: 'eu-west-1'});
 
         try {
             const snsParams = {
                 Subject: `Product was added`,
-                Message: JSON.stringify(message),
+                Message: JSON.stringify(messages),
                 TopicArn: CATALOG_ITEMS_ADD_SUBSCRIPTION,
+                MessageAttributes: {
+                    guitarPrice: {
+                        DataType: "Number",
+                        StringValue: messages[0].price
+                    }
+                }
             };
 
             await sns.publish(snsParams).promise();
