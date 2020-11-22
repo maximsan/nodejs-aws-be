679307d095359c4582b6c2a826b7cc61dff34a36
maximsan <maximuzz@yandex.ru>
2020-11-22 22:45:10 +0300

    fix: fix getProductsList.test.js

---
+++ product-service/__mocks__/products.js
@@ -0,0 +1,16 @@
+export const mockedProducts = [
+    {
+        "count": 4,
+        "description": "Short Product Description1",
+        "id": "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
+        "price": 2.4,
+        "title": "Guitar 1"
+    },
+    {
+        "count": 6,
+        "description": "Short Product Description3",
+        "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a0",
+        "price": 10,
+        "title": "Guitar 2"
+    },
+]
--- product-service/src/handlers/getProductsList.test.js
+++ product-service/src/handlers/getProductsList.test.js
@@ -1,15 +1,41 @@
 import lambdaTester from 'lambda-tester';
 import {getProductsList} from "./getProductsList";
-import productModel from '../../products.json';
+import {ProductService} from "../product.service";
+import {mockedProducts} from "../../__mocks__/products";
 
 describe('getProductsList', () => {
+    let serviceSpy
+    beforeEach(() => {
+        serviceSpy = jest.spyOn(ProductService.prototype, "getAll")
+    })
+
+    afterEach(() => {
+        serviceSpy.mockClear();
+    })
+
     it('should return list of product', async () => {
+        serviceSpy.mockReturnValue(mockedProducts);
+
+        const expectedResult = mockedProducts;
+
         await lambdaTester(getProductsList)
             .event({})
             .expectResult((result) => {
                 expect(result.statusCode).toBe(200);
-                const body = JSON.parse(result.body);
-                expect(body).toEqual(productModel);
+                expect(typeof result.body).toBe('string');
+                expect(() => JSON.parse(result.body)).not.toThrow();
+                expect(JSON.parse(result.body)).toEqual(expectedResult);
             });
     })
+    it('should return status code 500 if something went wrong', async () => {
+        serviceSpy.mockReturnValue(Promise.reject(new Error('Error!')));
+
+        await lambdaTester(getProductsList)
+            .event({})
+            .expectResult((result) => {
+                expect(result.statusCode).toBe(500);
+                console.log(result.body);
+                expect(typeof result.body).toBe('string');
+            });
+    });
 })
